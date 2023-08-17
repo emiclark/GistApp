b89f1cc7f79fe940e46b1b8753c03c45ef17baec
Clark, Emiko [CWM] <Emiko.Clark@ny.email.gs.com>
2023-08-17 11:35:01 -0400

    create basic URLSession

--- Gist/DataService.swift
+++ Gist/DataService.swift
@@ -6,8 +6,33 @@ class DataService {
   fileprivate let baseURLString = "https://api.github.com"
   
   func fetchGists() {
-    let url = createURLComponents(path: "/gists")
-    print(">> url: \(url)")
+    let componentURL = createURLComponents(path: "/gists/public")
+    guard let validURL = componentURL.url
+    else {
+      print(">> url failed")
+      return
+    }
+    
+    print(">> url: \(validURL)")
+    
+    URLSession.shared.dataTask(with: URLRequest(url: validURL)) { (data, response, error) in
+      if let response = response as? HTTPURLResponse {
+        print("response: \(response.statusCode)")
+      }
+      
+      guard let validData = data, error == nil
+      else {
+        print("error: \(String(describing: error?.localizedDescription))")
+        return
+      }
+
+      do {
+        let json = try JSONSerialization.jsonObject(with: validData)
+      } catch let error {
+        print(error)
+      }
+      
+    }.resume()
   }
   
   func createURLComponents(path: String) -> URLComponents {
