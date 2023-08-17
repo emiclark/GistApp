52044af6fc613b01b7fddd1151035592706bf8c5
Clark, Emiko [CWM] <Emiko.Clark@ny.email.gs.com>
2023-08-17 13:22:10 -0400

    Add decoder to model

--- Gist/Feature/Presentation/HomeScreen/HomeScreenModel.swift
+++ Gist/Feature/Presentation/HomeScreen/HomeScreenModel.swift
@@ -3,11 +3,18 @@ import Foundation
 struct Gist: Codable {
   var id: String
   var isPublic: Bool
+  var description: String
   
   enum CodingKeys: String, CodingKey {
-    case id, isPublic = "public"
+    case id, description, isPublic = "public"
+  }
+  
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    self.id = try container.decode(String.self, forKey: .id)
+    self.isPublic = try container.decode(Bool.self, forKey: .isPublic)
+    self.description = try container.decodeIfPresent(String.self, forKey: .description) ?? "Description is nil"
   }
 }
-
 
 
